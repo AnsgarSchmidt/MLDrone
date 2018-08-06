df2101c19df1ac10e0ce319ce8b29c627b109b06
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2018-08-06 21:03:11 +0200

    init version

---
+++ navigator/QRmamager.py
@@ -0,0 +1,9 @@
+import pyzbar.pyzbar as     pyzbar
+import numpy         as     np
+import time
+import cv2
+
+class QRmanager():
+
+    def __init__(self):
+        print("INIT")
---
+++ navigator/main.py
@@ -0,0 +1,13 @@
+import time
+from QRmamager import QRmanager
+
+class Main():
+
+    def __init__(self):
+        print ("INIT")
+        
+
+
+if __name__ == "__main__":
+    m = Main()
+    time.sleep(4)
