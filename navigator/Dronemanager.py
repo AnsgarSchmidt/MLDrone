5071d1bb821509af615b36da8492ad042dcde58e
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2018-08-14 18:23:20 +0200

    Stabilizing based on hard coded values but power externally controlled

--- navigator/Dronemanager.py
+++ navigator/Dronemanager.py
@@ -1,21 +1,89 @@
 from   Multiwii import MultiWii
 from   sys      import stdout
 import time
+import threading
 
 MAX_POWER      = 1999
+MIN_POWER      = 1000
+POWER_DELTA    = 30
 CENTER_X       = 0      # bigger -> right
-CENTER_Y       = -6.2     # bigger -> forkward
+CENTER_Y       = -2.2   # bigger -> forkward
 ALPHA          = 1
 MAX_CORRECTION = 100
 
+
+class Dronemanager(threading.Thread):
+
+    def __init__(self):
+        super(Dronemanager, self).__init__()
+        self.setDaemon(True)
+        self._shouldrun = False
+        self._center_x  = CENTER_X
+        self._center_y  = CENTER_Y
+        self._board     = MultiWii("/dev/ttyUSB0")
+        self._rc        = [1500,1500,1500,1000,1500]
+
+    def stop(self):
+        self._shouldrun = False
+
+    def up(self):
+        val = self._rc[3]
+        if val < (MAX_POWER - POWER_DELTA):
+            self._rc[3] = val + POWER_DELTA
+        else:
+            self._rc[3] = MAX_POWER
+
+    def down(self):
+        val = self._rc[3]
+        if val > (MIN_POWER + POWER_DELTA):
+            self._rc[3] = val - POWER_DELTA
+        else:
+            self._rc[3] = MIN_POWER
+
+    def left(self):
+        pass
+
+    def right(self):
+        pass
+
+    def run(self):
+        self._shouldrun = True
+        self._board.arm()
+        self._rc = [1500,1500,1500,1000,1500]
+
+        while self._shouldrun:
+            self._board.getData(MultiWii.ATTITUDE)
+            print ("Th:%04d - Roll:%04d - Pitch:%04d - X:%0.1f - Y:%0.1f" % (self._rc[3], self._rc[0], self._rc[1], self._board.attitude['angx'], self._board.attitude['angy'])) 
+            angx = self._board.attitude['angx']
+            angy = self._board.attitude['angy']
+            
+            # drifting right -> move left
+            if angx < (CENTER_X - ALPHA):   # right
+                self._rc[0] = 1500 + MAX_CORRECTION
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+            elif angx > (CENTER_X + ALPHA): # left
+                self._rc[0] = 1500 - MAX_CORRECTION
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+            else:
+                self._rc[0] = 1500
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+
+            if angy < CENTER_Y - ALPHA: 
+                self._rc[1] = 1500 + MAX_CORRECTION
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+            elif angy > CENTER_Y + ALPHA:
+                self._rc[1] = 1500 - MAX_CORRECTION
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+            else:
+                self._rc[1] = 1500
+                self._board.sendCMD(10, MultiWii.SET_RAW_RC, self._rc)
+
+        print("Shutting down")
+        self._board.disarm()
+
 if __name__ == "__main__":
-    board = MultiWii("/dev/ttyUSB0")
 
     try:
-
-        start = time.time()
-        state = 0
-        data = [1500,1500,1500,1000,1500]
 
         while True:
 
@@ -102,7 +170,6 @@ if __name__ == "__main__":
         data[3] = 1500
         board.sendCMD(10, MultiWii.SET_RAW_RC,data)
         time.sleep(1)
-        board.disarm()
         time.sleep(1)
     except Exception as error:        
         print ("Error on Main: " + str(error))
