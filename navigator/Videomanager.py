abfe0ea3cc9b8f951f36a9c1bf7c412542da93b1
Ansgar Schmidt <ansgar.schmidt@gmx.net>
2018-08-14 18:23:42 +0200

    Catching error when closeing stream

--- navigator/Videomanager.py
+++ navigator/Videomanager.py
@@ -24,7 +24,10 @@ class Videomanager(threading.Thread):
         
     def stop(self):
         self._shouldrun = False
-        self._camera.stop_recording()
+        try:
+            self._camera.stop_recording()
+        except:
+            pass
         now = datetime.datetime.now()
         directory = "%s/%04d-%02d-%02d" % (VIDEO_FOLDER, now.year, now.month, now.day)
         if not os.path.exists(directory):
@@ -33,9 +36,6 @@ class Videomanager(threading.Thread):
         for f in files:
             if f.endswith(".h264"):
                 shutil.move(TEMP_FOLDER + "/" + f, directory)
-
-        #move stuff
-
 
 if __name__ == "__main__":
     v = Videomanager()
